8fb253f68e16fe7259336ba5ecfe03991b8897cf
Sagar Mane <sagar.mane006@gmail.com>
2017-04-23 09:41:20 -0700

    Setting up Single Page Application structure with angular routing

---
+++ angular/main.js
@@ -0,0 +1,20 @@
+var app = angular.module('myapp', ['ngRoute']);
+app.config(function($routeProvider) {
+	console.log("in route provider");
+	$routeProvider
+	.when("/portfolio", {
+		templateUrl : "templates/portfolio.html",
+		controller : "portfolio_controller"
+	}).when("/photography", {
+		templateUrl : "templates/photography.html",
+		controller : "photography_controller"
+	});
+});
+
+app.controller("portfolio_controller",function($scope,$http){
+	console.log("Reporting from portfolio controller");
+});
+
+app.controller("photography_controller",function($scope){
+   console.log("Reporting from photography controller") 
+});
